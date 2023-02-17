a1559235f727364039e474c6f76ba54ae1befb2a
developer-sidani <developer.sidani@gmail.com>
2023-02-17 16:11:29 +0100

    feat: matrices stacking according to the colors and all stacking

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,5 +1,6 @@
 from .save_image import save_image
 from .svd import svd
 from .truncate import truncate
+from .matrices_stacking import *
 
-__all__ = ['save_image','svd', 'truncate']
+__all__ = ['save_image', 'svd', 'truncate', 'stack_matrices', 'stack_all_matrices']
