fd9116d273d41b173c4a5c1a725d5579813be91d
developer-sidani <developer.sidani@gmail.com>
2023-02-17 16:31:54 +0100

    feat: image compression evaluation according to peak signal to noise ratio, and the structural similarity index

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -2,5 +2,6 @@ from .save_image import save_image
 from .svd import svd
 from .truncate import truncate
 from .matrices_stacking import *
+from .compression_evaluation import *
 
-__all__ = ['save_image', 'svd', 'truncate', 'stack_matrices', 'stack_all_matrices']
+__all__ = ['save_image', 'svd', 'truncate', 'stack_matrices', 'stack_all_matrices', 'evaluate_psnr', 'evaluate_ssim']
