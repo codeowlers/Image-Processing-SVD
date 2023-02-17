b7518d06bbb89591592e3602fc0b5a81aade4e6d
Ali Yassine <aliyassine_2000@hotmail.com>
2023-02-17 15:07:38 +0100

    Get image size ondisk

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,3 +1,4 @@
 from .save_image import save_image
+from .svd import svd
 
-__all__ = ['save_image']
+__all__ = ['save_image','svd']
--- algorithms/save_image.py
+++ algorithms/save_image.py
@@ -1,7 +1,26 @@
 import time
 import cv2
-def save_image(data):
-    current_timestamp = int(time.time())
+import os
 
+def save_image(data, image_type, onlySize=False):
+    current_timestamp = int(time.time())
+    path = f'compressed-images/{current_timestamp}.{image_type}'
     # Save the compressed image
-    cv2.imwrite(f'compressed-images/{current_timestamp}.jpg', data)
+    # Save the compressed image
+    if image_type == 'jpg':
+        cv2.imwrite(path, data, [cv2.IMWRITE_JPEG_QUALITY, 80])
+    elif image_type == 'png':
+        cv2.imwrite(path, data, [cv2.IMWRITE_PNG_COMPRESSION, 3])
+    elif image_type == 'webp':
+        cv2.imwrite(path, data, [cv2.IMWRITE_WEBP_QUALITY, 80])
+    else:
+        raise ValueError(f'Invalid image format: {format}')
+
+    size = os.path.getsize(path)
+    # Convert size to MB
+    size = round(size / (1024.0 * 1024.0), 4)
+    if onlySize is True:
+        # Delete the saved image
+        os.remove(path)   
+
+    return size 
